c546dabec6c9c9268c3d4dffda8b07b213564444
farid <jhonnydep813@gmail.com>
2023-07-25 14:38:37 +0200

    viewconnectionphp

---
+++ style/view/view_connexion.php
@@ -0,0 +1,15 @@
+<html lang="fr">
+    <head>
+        <meta charset ="UTF-8">
+</head>
+<body>
+    <h1>Connexion : </h1>
+    <form action="" method="post">
+        <p>Veuillez saisir votre identifiant :</p>
+        <input type="text" name="login">
+        <p>Veuillez saisir votre mot de passe :</p>
+        <input type="text" name="mdp">
+        <input type="submit" value="connexion">
+</form>
+</body>
+</html>
